8da8fc3c64243024b78137bb104063725e405827
Keith Linneman <keith@linnemanlabs.com>
2026-02-24 04:33:58 -0500

    add nil check

--- internal/alertapi/api.go
+++ internal/alertapi/api.go
@@ -13,22 +13,27 @@ import (
 type API struct {
 	logger log.Logger
 	store  *triage.Store
+	engine *triage.Engine
 	// triage service
 	// alerts service
 }
 
 // New creates a new API handler.
 // logger may be nil, in which case a no-op logger is used
-func New(logger log.Logger, store *triage.Store) *API {
+func New(logger log.Logger, store *triage.Store, engine *triage.Engine) *API {
 	if logger == nil {
 		logger = log.Nop()
 	}
 	if store == nil {
 		panic(xerrors.New("triage store is required"))
 	}
+	if engine == nil {
+		panic(xerrors.New("triage engine is required"))
+	}
 	return &API{
 		logger: logger,
 		store:  store,
+		engine: engine,
 	}
 }
 
